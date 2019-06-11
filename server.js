e96b573dd4ef4474246b1fa36ac1e98f798b4003
Morgana Spake <morgana.spake@gmail.com>
2019-06-11 12:08:38 -0700

    reafactored to allow multiple games at once

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 
 const events = require('./events.js');
+const Game = require('./game.js');
 
 const io = require('socket.io')(process.env.PORT);
 
@@ -10,17 +11,7 @@ const io = require('socket.io')(process.env.PORT);
 //Global Variables
 //===========================================
 
-// Chris - a tally to keep track of how many items are left game-wide accross all stacks. 
-// When this is zero, game is over.
-let totalItemsRemaining;
-
-// Chris - Here's a basic setup for stacks to use during the game.
-//Morgana - switched it over to being an object and using dynamic amounts.
-let stacks = {
-  a: generateRandomAmount(),
-  b: generateRandomAmount(),
-  c: generateRandomAmount(),
-};
+let games = {};
 
 let players = [];
 
@@ -30,6 +21,7 @@ let players = [];
 
 io.on('connect', (socket) => {
   console.log(`Socket ${socket.id} connected`);
+  
   players.push(socket.id);
 
   if(players.length === 1) {
@@ -37,29 +29,48 @@ io.on('connect', (socket) => {
   }
 
   if(players.length === 2) {
-    io.to(`${players[0]}`).emit(events.message, 'Starting Game');
-    io.to(`${players[1]}`).emit(events.message, 'Starting Game');
-    io.to(`${players[1]}`).emit(events.message, 'Waiting for other player to move');
-    io.to(`${players[0]}`).emit(events.turn, stacks);
+    //Morgana - When there are two players, create a new game instance with an id
+    let gameInstance = new Game(Object.keys(games).length);
+    //Morgana - Set the players, and save the game to the games object
+    //Morgana - spread operator creates a shallow copy, avoiding reference issues
+    gameInstance.players = [...players];
+    console.log(gameInstance);
+    games[gameInstance.id] = gameInstance;
+    console.log(games);
+    
+    //Morgana - send the initial game-start messages
+    io.to(`${gameInstance.players[0]}`).emit(events.message, 'Starting Game');
+    io.to(`${gameInstance.players[1]}`).emit(events.message, 'Starting Game');
+    io.to(`${gameInstance.players[1]}`).emit(events.message, 'Waiting for other player to move');
+    io.to(`${gameInstance.players[0]}`).emit(events.turn, [gameInstance.id, gameInstance.stacks]);
+
+    //Morgana - empty the players array
+    players = [];
   }
 
   socket.on(events.move, payload => {
-    let playerWhoMoved = players.indexOf(socket.id);
+    //retrive game id
+    const gameID = payload[0];
+    const stackChoice = payload[1];
+    const numberToTake = payload[2];
+    const currentGame = games[gameID];
+    let playerWhoMoved = currentGame.players.indexOf(socket.id);
     
     if(playerWhoMoved === 0) {
-      gameCycle(payload[0], payload[1]);
-      io.to(`${players[1]}`).emit(events.turn, stacks);
-      io.to(`${players[0]}`).emit(events.message, 'Waiting for other player to move');
+      gameCycle(currentGame, stackChoice, numberToTake);
+      io.to(`${currentGame.players[1]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
+      io.to(`${currentGame.players[0]}`).emit(events.message, 'Waiting for other player to move');
       
     }
     else if(playerWhoMoved === 1) {
-      gameCycle(payload[0], payload[1]);
-      io.to(`${players[0]}`).emit(events.turn, stacks);
-      io.to(`${players[1]}`).emit(events.message, 'Waiting for other player to move');
+      gameCycle(currentGame, stackChoice, numberToTake);
+      io.to(`${currentGame.players[0]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
+      io.to(`${currentGame.players[1]}`).emit(events.message, 'Waiting for other player to move');
     }
   });
 
   socket.on('disconnect', () => {
+    //retrieve game id?
     console.log(`Socket ${socket.id} disconnected`);
     players.splice(players.indexOf[socket.id], 1);
     if(players.length === 1) {
@@ -75,46 +86,19 @@ io.on('connect', (socket) => {
 //===========================================
 
 //Morgana - refactored to handle new stack structure and gameover emits
-const gameCycle = (stackChoice, numberToTake) => {
-  // if(checkChoices(stackChoice, numberToTake) === true) {
-  takeItemsFromStack(stackChoice, numberToTake);
-  // }
-  if(totalItemsRemaining !== 0) {
-    console.log({totalItemsRemaining});
+const gameCycle = (currentGame, stackChoice, numberToTake) => {
+
+  currentGame.takeItemsFromStack(stackChoice, numberToTake);
+
+  if(currentGame.totalItemsRemaining !== 0) {
+    console.log('Total remaining', currentGame.totalItemsRemaining);
   }
   else {
     console.log('GAME OVER!!!!!!!');
-    io.to(`${players[0]}`).emit(events.gameOver, 'Game Over!');
-    io.to(`${players[1]}`).emit(events.gameOver, 'Game Over!');
-    players = [];
-    stacks = {
-      a: generateRandomAmount(),
-      b: generateRandomAmount(),
-      c: generateRandomAmount(),
-    };
+    io.to(`${currentGame.players[0]}`).emit(events.gameOver, 'Game Over!');
+    io.to(`${currentGame.players[1]}`).emit(events.gameOver, 'Game Over!');
   }
 };
 
-//Morgana - generates a random amount between 5 and 25
-function generateRandomAmount() {
-  return Math.floor(Math.random() * 20) + 5;
-}
-
-// Chris - this function tallies a new total for totalItemsRemaining after a player makes a valid move.
-// Morgana - adjusted for new stack structure
-const _tallyTotalItemsRemaining = () => {
-  let total = 0;
-  for(let i in stacks) {
-    total += stacks[i];
-  }
-  totalItemsRemaining = total;
-};
-
-// Chris - This applies the player's move to the stack they selected, and updates totalRemainingItems.
-// Morgana - adjusted for new stack structure
-const takeItemsFromStack = (stackChoice, numberToTake) => {
-  stacks[stackChoice] = stacks[stackChoice] - numberToTake;
-  _tallyTotalItemsRemaining();
-};
 
 
