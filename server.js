a5c558258b1878c66f7bda1ba0df69288133ad8a
Morgana Spake <morgana.spake@gmail.com>
2019-06-12 08:03:05 -0700

    sends a win event

--- server.js
+++ server.js
@@ -58,13 +58,13 @@ io.on('connect', (socket) => {
     let playerWhoMoved = currentGame.players.indexOf(socket.id);
     
     if(playerWhoMoved === 0) {
-      gameCycle(currentGame, stackChoice, numberToTake);
+      gameCycle(currentGame, stackChoice, numberToTake, socket.id);
       io.to(`${currentGame.players[1]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
       io.to(`${currentGame.players[0]}`).emit(events.message, 'Waiting for other player to move');
       
     }
     else if(playerWhoMoved === 1) {
-      gameCycle(currentGame, stackChoice, numberToTake);
+      gameCycle(currentGame, stackChoice, numberToTake, socket.id);
       io.to(`${currentGame.players[0]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
       io.to(`${currentGame.players[1]}`).emit(events.message, 'Waiting for other player to move');
     }
@@ -93,7 +93,7 @@ io.on('connect', (socket) => {
 //===========================================
 
 //Morgana - refactored to handle new stack structure and gameover emits
-const gameCycle = (currentGame, stackChoice, numberToTake) => {
+const gameCycle = (currentGame, stackChoice, numberToTake, socketID) => {
 
   currentGame.takeItemsFromStack(stackChoice, numberToTake);
 
@@ -102,6 +102,8 @@ const gameCycle = (currentGame, stackChoice, numberToTake) => {
   }
   else {
     console.log('GAME OVER!!!!!!!');
+    //Morgana - pass win back to client
+    io.to(`${socketID}`).emit(events.win);
     io.to(`${currentGame.players[0]}`).emit(events.gameOver, 'Game Over!');
     io.to(`${currentGame.players[1]}`).emit(events.gameOver, 'Game Over!');
   }
