8b744743aa8211c8457e9b8c90eb9e6b712f9fa4
Morgana Spake <40220209+MSpake@users.noreply.github.com>
2019-06-13 17:34:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -24,29 +24,6 @@ function connectionContainer(socket) {
   gamesHolder = games;
 
   socket.on(events.move, payload => {
-//     const gameID = payload[0];
-//     const stackChoice = payload[1];
-//     const numberToTake = payload[2];
-//     const currentGame = games[gameID];
-//     clearInterval(currentGame.countdown);
-//     currentGame.timeLeft = 20;
-//     let playerWhoMoved = currentGame.players.indexOf(socket.id);
-    
-//     if(playerWhoMoved === 0) {
-//       const otherPlayer = currentGame.players[1];
-//       gameCycle(currentGame, stackChoice, numberToTake, otherPlayer);
-//       io.to(`${currentGame.players[1]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
-//       currentGame.countdown = setInterval(currentGame.decrement, 1000, currentGame.players[1]);
-//       io.to(`${currentGame.players[0]}`).emit(events.message, 'Waiting for other player to move');  
-//     }
-//     else if(playerWhoMoved === 1) {
-//       const otherPlayer = currentGame.players[0];
-//       gameCycle(currentGame, stackChoice, numberToTake, otherPlayer);
-//       currentGame.countdown = setInterval(currentGame.decrement, 1000, currentGame.players[0]);
-//       io.to(`${currentGame.players[0]}`).emit(events.turn, [currentGame.id, currentGame.stacks]);
-//       io.to(`${currentGame.players[1]}`).emit(events.message, 'Waiting for other player to move');
-//     }
-
     moveWrapper(socket, gamesHolder, payload);
   });
 
